bc2744d8d6579c1d9c8ef321f3607ffc6544296f
Majid Joseph <majid.joseph7@gmail.com>
2019-12-30 01:26:51 -0500

    styling welcome page

--- Screens/Login/index.js
+++ Screens/Login/index.js
@@ -1,146 +1,198 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import s from './style';
-import {View, Text, Button} from 'react-native';
+import { View, Text, Button, TextInput, KeyboardAvoidingView, TouchableOpacity, Image } from 'react-native';
 import * as Google from 'expo-google-app-auth';
 import firebase from 'firebase';
 import * as Facebook from 'expo-facebook';
 
-export default class Login extends Component{
+export default class Login extends Component {
 
-    isUserEqual = (googleUser, firebaseUser) => {
-        if (firebaseUser) {
-            var providerData = firebaseUser.providerData;
-            for (var i = 0; i < providerData.length; i++) {
-                if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-                    providerData[i].uid === googleUser.getBasicProfile().getId()) {
-                    // We don't need to reauth the Firebase connection.
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+  state = {
+    email: '',
+    password: '',
+  }
 
-    onSignIn = googleUser => {
-        console.log('Google Auth Response', googleUser);
-        // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-        var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
-            unsubscribe();
-            // Check if we are already signed-in Firebase with the correct user.
-            if (!this.isUserEqual(googleUser, firebaseUser)) {
-                // Build Firebase credential with the Google ID token.
-                var credential = firebase.auth.GoogleAuthProvider.credential(
-                    googleUser.idToken,
-                    googleUser.accessToken
-                );
-                // Sign in with credential from the Google user.
-                firebase.auth().signInWithCredential(credential).then(function (result) {
-                    console.log('User Signed In');
-                    if (result.additionalUserInfo.isNewUser) {
-                        firebase
-                          .database()
-                          .ref('/users/' + result.user.uid)
-                          .set({
-                            email: result.user.email,
-                            profilePicture: result.additionalUserInfo.profile.picture,
-                            firstName: result.additionalUserInfo.profile.given_name,
-                            lastName: result.additionalUserInfo.profile.family_name,
-                            created_at: Date.now()
-                          })
-                          .then(function(snapshot) {
-                              console.log('Snapshot', snapshot);
-                          });
-                      } else {
-                        firebase
-                          .database()
-                          .ref('/users/' + result.user.uid)
-                          .update({
-                            last_logged_in: Date.now()
-                          });
-                      }
-                })
-                    .catch(function (error) {
-                        // Handle Errors here.
-                        var errorCode = error.code;
-                        var errorMessage = error.message;
-                        // The email of the user's account used.
-                        var email = error.email;
-                        // The firebase.auth.AuthCredential type that was used.
-                        var credential = error.credential;
-                        // ...
-                    });
-            } else {
-                console.log('User already signed-in Firebase.');
-            }
-        }.bind(this));
-    }
-
-    signInWithGoogleAsync = async() =>{
-        try {
-          const result = await Google.logInAsync({
-            // androidClientId: YOUR_CLIENT_ID_HERE,
-            iosClientId: '779486166693-h4l05uppjr72j16eht8hoj4e63eelb0g.apps.googleusercontent.com',
-            scopes: ['profile', 'email'],
-          });
-      
-          if (result.type === 'success') {
-            this.onSignIn(result);
-            return result.accessToken;
-          } else {
-            return { cancelled: true };
-          }
-        } catch (e) {
-          return { error: true };
+  isUserEqual = (googleUser, firebaseUser) => {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+          return true;
         }
       }
-
-    signInWithFacebookAsync = async () =>{
-      await Facebook.initializeAsync('2442362725891867');
-      const { type, token } = await Facebook.logInWithReadPermissionsAsync('2442362725891867', { permissions: ['public_profile'] });
-      
-      if(type == 'success'){
-        const credential = firebase.auth.FacebookAuthProvider.credential(token);
-
-        firebase.auth().signInWithCredential(credential).then(function (result) {
-          if (result.additionalUserInfo.isNewUser) {
-            firebase.database().ref('/users/' + result.user.uid)
-                    .set({
-                        email: result.user.email,
-                        profilePicture: result.user.photoURL,
-                        firstName: result.additionalUserInfo.profile.first_name,
-                        lastName: result.additionalUserInfo.profile.last_name,
-                        created_at: Date.now()
-                      }).then(function(snapshot){});
-          }
-          else{
-            firebase.database().ref('/users/' + result.user.uid).update({last_logged_in: Date.now()});
-          }
-          }).catch(function (error) {
-              console.log('error');
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // The email of the user's account used.
-              var email = error.email;
-              // The firebase.auth.AuthCredential type that was used.
-              var credential = error.credential;
-              // ...
-          });
-      }
     }
+    return false;
+  }
 
-    render(){
-        return(
-            <View style={s.container}>
-                <Button 
-                    title="Sign in with Google"
-                    onPress = {() => this.signInWithGoogleAsync()}
-                />
-                <Button 
-                    title="Sign in with Facebook"
-                    onPress = {() => this.signInWithFacebookAsync()}
-                />
-            </View>
+  onSignIn = googleUser => {
+    console.log('Google Auth Response', googleUser);
+    var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
+      unsubscribe();
+      if (!this.isUserEqual(googleUser, firebaseUser)) {
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.idToken,
+          googleUser.accessToken
         );
+        firebase.auth().signInWithCredential(credential).then(function (result) {
+          console.log('User Signed In');
+          if (result.additionalUserInfo.isNewUser) {
+            firebase
+              .database()
+              .ref('/users/' + result.user.uid)
+              .set({
+                email: result.user.email,
+                profilePicture: result.additionalUserInfo.profile.picture,
+                firstName: result.additionalUserInfo.profile.given_name,
+                lastName: result.additionalUserInfo.profile.family_name,
+                created_at: Date.now()
+              })
+              .then(function (snapshot) {
+                console.log('Snapshot', snapshot);
+              });
+          } else {
+            firebase
+              .database()
+              .ref('/users/' + result.user.uid)
+              .update({
+                last_logged_in: Date.now()
+              });
+          }
+        })
+          .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+          });
+      } else {
+        console.log('User already signed-in Firebase.');
+      }
+    }.bind(this));
+  }
+
+  signInWithGoogleAsync = async () => {
+    try {
+      const result = await Google.logInAsync({
+        // androidClientId: YOUR_CLIENT_ID_HERE,
+        iosClientId: '779486166693-h4l05uppjr72j16eht8hoj4e63eelb0g.apps.googleusercontent.com',
+        scopes: ['profile', 'email'],
+      });
+
+      if (result.type === 'success') {
+        this.onSignIn(result);
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
     }
+  }
+
+  signInWithFacebookAsync = async () => {
+    await Facebook.initializeAsync('2442362725891867');
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync('2442362725891867', { permissions: ['public_profile'] });
+
+    if (type == 'success') {
+      const credential = firebase.auth.FacebookAuthProvider.credential(token);
+
+      firebase.auth().signInWithCredential(credential).then(function (result) {
+        if (result.additionalUserInfo.isNewUser) {
+          firebase.database().ref('/users/' + result.user.uid)
+            .set({
+              email: result.user.email,
+              profilePicture: result.user.photoURL,
+              firstName: result.additionalUserInfo.profile.first_name,
+              lastName: result.additionalUserInfo.profile.last_name,
+              created_at: Date.now()
+            }).then(function (snapshot) { });
+        }
+        else {
+          firebase.database().ref('/users/' + result.user.uid).update({ last_logged_in: Date.now() });
+        }
+      }).catch(function (error) {
+        console.log('error');
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+      });
+    }
+  }
+
+  // render() {
+  //   return (
+  //     <View style={s.container}>
+  //       <View style={s.titleContainer}>
+  //         <Text style={s.title}>Reminder</Text>
+  //       </View>
+  //       <KeyboardAvoidingView style={s.bodyContainer}>
+  //         <TextInput
+  //           style={s.input}
+  //           maxLength={25}
+  //           placeholder='Email'
+  //           placeholderTextColor='white'
+  //           keyboardAppearance='dark'
+  //           label='Email'
+  //           value={this.state.email}
+  //           onChangeText={email => this.setState({ email })}
+  //         />
+  //         <TextInput
+  //           secureTextEntry={true}
+  //           style={s.input}
+  //           maxLength={25}
+  //           placeholder="Password"
+  //           placeholderTextColor='white'
+  //           keyboardAppearance='dark'
+  //           label='Password'
+  //           value={this.state.password}
+  //           onChangeText={password => this.setState({ password })}
+  //         />
+  //         <Button 
+  //           title="Log In"
+  //           onPress = {() => alert('Button Pressed')}
+  //         />
+  //         <Text>New to Reminder? Sign Up!</Text>
+  //         <Button
+  //           title="Sign in with Google"
+  //           onPress={() => this.signInWithGoogleAsync()}
+  //         />
+  //         <Button
+  //           title="Sign in with Facebook"
+  //           onPress={() => this.signInWithFacebookAsync()}
+  //         />
+  //       </KeyboardAvoidingView>
+  //     </View>
+  //   );
+  render() {
+    return (
+      <View style={s.container}>
+        <View style={s.topContainer}>
+          <Image
+            source={require('../../assets/Images/logo.png')}
+            style={s.logo}
+          />
+        </View>
+        <View style={s.floatContainer}>
+          <Text style={s.welcomeText}>Welcome to Reminder!</Text>
+          <TouchableOpacity style={s.signInButton}>
+            <Text style={s.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={s.signUpButton}>
+            <Text style={s.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+          <View style={s.hrContainer}>
+            <View style={s.line}></View>
+            <Text style={s.orText}>Or connect using</Text>
+            <View style={s.line}></View>
+          </View>
+          <View style={s.connectContainer}>
+            <TouchableOpacity><Text>Sign in with Google</Text></TouchableOpacity>
+            <TouchableOpacity><Text>Sign in with Facebook</Text></TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
